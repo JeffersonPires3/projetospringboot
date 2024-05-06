4eaeff0e9ce6432115a8cd3e82cab8e8078414de
JeffersonPires3 <jef.pires@gmail.com>
2024-05-06 04:45:16 -0300

    Add files via upload

---
+++ ProjetoResidenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_residencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoResidenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoResidenciaApplication.class, args);
+	}
+
+}
